ebcd40796d4e712ac43c32a45253386ec642b6f2
Caroline Genzel <carolinegenzel@MacBook-Pro-sl-Caroline.local>
2024-11-05 23:04:54 +0200

    shorter code

--- Ascend.java
+++ Ascend.java
@@ -6,15 +6,13 @@ public class Ascend {
 		int a= (int) (Math.random() * lim);
 		int b= (int) (Math.random() * lim);
 		int c= (int) (Math.random() * lim);
-		int minNumber1 = Math.min(a, b);
-		int minNumber2= Math.min(b, c);
-		int minNum= Math.min(minNumber1, minNumber2);
-		int middleNum= Math.max(minNumber1, minNumber2);
-		int maxNum1=Math.max(a, b);
-		int maxNum2= Math.max(b, c);
-		int maxNum= Math.max(maxNum2, maxNum1);
-		System.out.println(minNum +" "+ middleNum +" "+ maxNum);
-		System.out.println(maxNum+" "+ middleNum +" "+ minNum);
+		int minNumber = Math.min(Math.min(a, b), c);
+		int maxNumber=Math.max(Math.max(a, b), c);
+		int middleNum= a+b+c-minNumber-maxNumber;
+		
+		System.out.println(maxNumber+" "+ middleNum +" "+ minNumber);
+		System.out.println(minNumber +" "+ middleNum +" "+ maxNumber);
+		
 
 	}
 }
